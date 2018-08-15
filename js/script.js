394dd1a3477f967657a021aef11cb51a3cae9674
Yurii-Adamenko <adamyura97@gmail.com>
2018-08-16 00:45:37 +0300

    created navbar

---
+++ js/script.js
@@ -0,0 +1,17 @@
+(function ($) {  
+
+  $(window).scroll(
+    function(){
+    var t_scroll = $(window).scrollTop();
+    var t_sticky = $('#navbar');
+
+    if (t_scroll >= 100) t_sticky.addClass('header-navbar--modifier');
+      else t_sticky.removeClass('header-navbar--modifier');
+  });
+
+  AOS.init({
+    duration: 500,
+    easing: 'ease-in-sine',
+  });
+
+})(jQuery);
